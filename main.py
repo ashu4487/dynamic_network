e2acacd8ab83f3acae4b7001ce0d38a916e78706
ashu <ashu@192.168.3.5>
2017-07-06 11:53:18 +0900

    corrected number of frames problem

--- main.py
+++ main.py
@@ -25,8 +25,10 @@ if os.path.isfile("all_contacts.txt"):
 else:
     print("Fresh run")
     all_contacts=set()                                      #create empty set to keep all contacts
+    num_frames=0
 #    for i in range(0,10000,int(skip)):
-    for i in range(int(b),int(e),int(skip)):    
+    for i in range(int(b),int(e),int(skip)):
+        num_frames= num_frames+1   
         fname=create_network.extract_frame(xtc,tpr,ndx,i) #Extracts frame and saves the PDB file with fname
         contacts=create_network.find_contact(fname) # Find the contacts ib protein structure
         outf=open(fname[:-4]+'contacts.txt','wb')
@@ -46,13 +48,13 @@ else:
 
     
 if os.path.isfile("contact_matrix.txt"):
-    print("Contact matrix already exixts in folder. Now reading it")
+    print("Contact matrix already exists in folder. Now reading it")
     contact_mat=np.loadtxt('contact_matrix.txt')
     
 else:
-    contact_mat=np.zeros([len(all_contacts),100])
+    contact_mat=np.zeros([len(all_contacts),num_frames])
     count=0
-    for i1 in range(0,10000,int(skip)):
+    for i1 in range(int(b),int(e),int(skip)):
         with open('frame'+str(i1)+'contacts.txt','rb') as rcon:
             con_read=pickle.load(rcon)
         for item in con_read:
@@ -63,7 +65,7 @@ else:
     np.savetxt('contact_matrix.txt',contact_mat,fmt="%d")
     
 #plt.imshow(contact_mat,aspect="auto",interpolation='nearest')
-[num_cont,num_frames]=np.shape(contact_mat)
+#[num_cont,num_frames]=np.shape(contact_mat)
 sum_contact=np.sum(contact_mat,axis=1)
 cont_prob=sum_contact/num_frames    #Calculate contact probability
 plt.plot(range(len(cont_prob)),sorted(cont_prob,reverse=True))
